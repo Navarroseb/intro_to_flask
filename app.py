476b1913b27d730aca78aba6653876eb87f7897c
Navarroseb <sebastnavarro@gmail.com>
2023-08-23 19:12:53 -0400

    intro to flask

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+import json
+from flask import Flask, jsonify, request 
+from dotenv import load_dotenv 
+
+##cargar variables de entorno
+load_dotenv()
+
+app = Flask(__name__)
+## configuración
+
+app.config['DEBUG'] = True
+app.config['ENV'] = "development"
+
+##code here
+
+@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def main():
+    saludo = {
+        "mesaje": "Hola desde mi primera API con Flask"
+    }
+    return jsonify(saludo)
+
+@app.route('/<name>/<int:age>', methods=['GET'])
+def saludo(name, age):
+    mensaje= {
+        "msg": f"Hola, soy {name} y tengo {age} años"
+    }    
+    
+app.route('/api/search/<category>', methods=['POST', 'PUT'])
+def search_post_put(category):
+
+    params = request.args
+
+    print(params)
+    print(request.data)
+
+    datos_en_json = request.get_jason()
+
+    return jsonify({ 
+                    "msg": "buscando",
+                    "category": category,
+                    "params": params,
+                    "method": request.method,
+                    "datos": datos_en_json
+    })    
+
+
+## fin code
+if __name__ == '__main__':
+    app.run()
